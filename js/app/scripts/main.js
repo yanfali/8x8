bce00046027790af3bccea8550efb7a3c7dd264b
Yan-Fa li <yanfali@gmail.com>
2014-04-30 22:17:22 -0700

    Basic coloring of grid elements
    You can now design basic 8x8 grids of pixels using 3 colors.
    
    This is the minimum viable jquery. I haven't decided whether it's worth
    importing backbone.
    
    - add lodash
    - use basic jquery to control background colors of tds
    - populate a model, an array of 64 values with color values

--- js/app/scripts/main.js
+++ js/app/scripts/main.js
@@ -1,1 +1,37 @@
-console.log('\'Allo \'Allo!');
+/* global _ */
+(function() {
+    'use strict';
+    $(function() {
+        var colors = ['none', 'Chartreuse', 'red', 'yellow'];
+        var model = _.map(_.range(64), function() {
+            return 0;
+        });
+        console.log(model);
+        $('table td').click(function(evt) {
+            var $target = $(evt.target);
+            var row = parseInt($target.closest('tr').attr('data-row'), 10);
+            var col = parseInt($target.attr('data-col'), 10);
+            var item = (row * 8 + col);
+            var color = 'none';
+            switch (model[item]) {
+            case 0:
+                color = 1;
+                break;
+            case 1:
+                color = 2;
+                break;
+            case 2:
+                color = 3;
+                break;
+            default:
+                color = 0;
+            }
+	    model[item] = color;
+            $target.css('background', colors[model[item]]);
+            console.log('row: ' + row + ' col: ' + col + ' item: ' + item);
+        });
+        window.eightbyeight = {
+            model: model
+        };
+    });
+})();
