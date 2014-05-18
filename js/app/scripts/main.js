5dc1b96c5211ac8a4cb0f2267a825e1b817e8d38
Yan-Fa li <yanfali@gmail.com>
2014-05-17 22:13:57 -0700

    Generalize rotation
    - add font-awesome to buttons
    - extract rotation function
    - implement clockwise and counter clockwise using a function
    - replace magic numbers

--- js/app/scripts/main.js
+++ js/app/scripts/main.js
@@ -2,18 +2,21 @@
 (function() {
     'use strict';
     $(function() {
-        function resetModel() {
-            return _.map(_.range(64), function() {
+        var width = 8,
+            height = 8;
+
+        function resetModel(width, height) {
+            return _.map(_.range(width * height), function() {
                 return 0;
             });
         }
         var colors = ['none', 'Chartreuse', 'red', 'yellow'];
-        var model = resetModel();
+        var model = resetModel(width, height);
 
-        function buildTableBody() {
+        function buildTableBody(width, height) {
             var $tbody = $('tbody');
             var trTemplate = _.template($('#tr-template').text());
-            var markup = _.reduce([0, 8, 16, 24, 32, 40, 48, 56], function(result, value, index) {
+            var markup = _.reduce(_.range(0, (width * height) - 1, width), function(result, value, index) {
                 result.push(trTemplate({
                     row: index,
                     offset: value
@@ -23,25 +26,25 @@
             $tbody.append(markup);
         }
 
-        buildTableBody();
+        buildTableBody(width, height);
 
         function render(model) {
             var row, col, color;
-            for (row = 0; row < 8; row++) {
+            for (row = 0; row < width; row++) {
                 var $row = $('tr[data-row="' + row + '"]');
-                for (col = 0; col < 8; col++) {
+                for (col = 0; col < height; col++) {
                     var $el = $row.find('td[data-col="' + col + '"]');
-                    color = colors[model[row * 8 + col]];
+                    color = colors[model[row * width + col]];
                     $el.css('background', color);
                 }
             }
         }
-        console.log(model);
+
         $('table td').bind('click', function(evt) {
             var $target = $(evt.target);
             var row = parseInt($target.closest('tr').attr('data-row'), 10);
             var col = parseInt($target.attr('data-col'), 10);
-            var item = (row * 8 + col);
+            var item = (row * width + col);
             var color = 'none';
             switch (model[item]) {
             case 0:
@@ -62,40 +65,59 @@
         });
 
         function calcPos(x, y) {
-            return y * 8 + x;
+            return y * width + x;
         }
 
-        $('.rotate').bind('click', _.debounce(function(evt) {
-            evt.preventDefault();
+        function rotate(fn) {
             var newModel = [];
-            var p1 = 0,
-                p2 = 0,
-                p3 = 0,
-                p4 = 0;
-            for (var o = 0; o < 4; o++) {
-                var x, y, x1, y1, x2, y2, x3, y3;
+            var x, y, x1, y1, x2, y2, x3, y3, p1, p2, p3, p4;
+            var rowEnd = width - 1;
+            for (var o = 0; o < width / 2; o++) {
                 x = y = y1 = x3 = o;
-	       	x1 = x2 = y2 = y3 = 7 - o;
-                for (var i = 0; i < (8 - o); i++) {
+                x1 = x2 = y2 = y3 = rowEnd - o;
+                for (var i = 0; i < (width - o); i++) {
                     p1 = calcPos(x + i, y);
                     p2 = calcPos(x1, y1 + i);
                     p3 = calcPos(x2 - i, y2);
                     p4 = calcPos(x3, y3 - i);
-                    newModel[p1] = model[p4];
-                    newModel[p2] = model[p1];
-                    newModel[p3] = model[p2];
-                    newModel[p4] = model[p3];
+                    fn(newModel, model, [p1, p2, p3, p4]);
                 }
             }
-            model = newModel;
+            return newModel;
+        }
+
+        function clockwise(newModel, oldModel, pos) {
+            newModel[pos[0]] = oldModel[pos[3]];
+            newModel[pos[1]] = oldModel[pos[0]];
+            newModel[pos[2]] = oldModel[pos[1]];
+            newModel[pos[3]] = oldModel[pos[2]];
+        }
+
+        function counterClockwise(newModel, oldModel, pos) {
+            newModel[pos[0]] = oldModel[pos[1]];
+            newModel[pos[1]] = oldModel[pos[2]];
+            newModel[pos[2]] = oldModel[pos[3]];
+            newModel[pos[3]] = oldModel[pos[0]];
+        }
+
+        $('.rotate-right').bind('click', _.debounce(function(evt) {
+            evt.preventDefault();
+            model = rotate(clockwise);
             render(model);
         }, 50));
+
+        $('.rotate-left').bind('click', _.debounce(function(evt) {
+            evt.preventDefault();
+            model = rotate(counterClockwise);
+            render(model);
+        }, 50));
+
         $('.clear').bind('click', function(evt) {
             evt.preventDefault();
-            console.log('reset');
-            model = resetModel();
+            model = resetModel(width, height);
             render(model);
         });
+
         window.eightbyeight = {
             model: model
         };
